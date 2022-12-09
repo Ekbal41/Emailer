21928b38ec8301b2e6305f2e49ebd7ce8ec8097a
Ekbal41 <mdasifekbalshagor@gmail.com>
2022-12-09 19:24:10 +0600

    gitignore added

--- emailproject/settings.py
+++ emailproject/settings.py
@@ -121,12 +121,12 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+from decouple import config
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mdasifekbalshagor@gmail.com'
-EMAIL_HOST_PASSWORD ='zzegkwrhreqxsvpn'
+EMAIL_HOST_USER = config('MYMAIL')
+EMAIL_HOST_PASSWORD = config('PASSWORD')
 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
